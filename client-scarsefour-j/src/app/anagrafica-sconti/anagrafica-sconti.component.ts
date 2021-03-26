6bcd415e4d3ebe4a924eb98f7211060f05adc13f
Valerio1990 <valeriopiccioni1990@gmail.com>
2021-03-26 17:31:30 +0100

    #21

---
+++ client-scarsefour-j/src/app/anagrafica-sconti/anagrafica-sconti.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anagrafica-sconti',
+  templateUrl: './anagrafica-sconti.component.html',
+  styleUrls: ['./anagrafica-sconti.component.css']
+})
+export class AnagraficaScontiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
