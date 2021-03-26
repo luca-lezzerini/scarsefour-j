fee89c2c13cfc882e076a4751fe1cd21abe70f1e
Valerio1990 <valeriopiccioni1990@gmail.com>
2021-03-26 16:47:57 +0100

    #8

--- client-scarsefour-j/src/app/app.module.ts
+++ client-scarsefour-j/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AnagraficaProdottiComponent } from './anagrafica-prodotti/anagrafica-prodotti.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AnagraficaProdottiComponent } from './anagrafica-prodotti/anagrafica-pr
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
