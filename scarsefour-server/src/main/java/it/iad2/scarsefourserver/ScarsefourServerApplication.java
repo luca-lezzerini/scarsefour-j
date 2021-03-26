4d230fcf4e588f203477716eb39986763ebfe06d
Matteosalvatori4 <matteo.salvatori4@gmail.com>
2021-03-26 15:53:24 +0100

    Creazione server vuoto #1
    Creazione server vuoto #1

---
+++ scarsefour-server/src/main/java/it/iad2/scarsefourserver/ScarsefourServerApplication.java
@@ -0,0 +1,13 @@
+package it.iad2.scarsefourserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScarsefourServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScarsefourServerApplication.class, args);
+	}
+
+}
---
+++ scarsefour-server/src/test/java/it/iad2/scarsefourserver/ScarsefourServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.iad2.scarsefourserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScarsefourServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
