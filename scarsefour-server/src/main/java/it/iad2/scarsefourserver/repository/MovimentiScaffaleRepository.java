0f2837910255c0e57f286e1085bb1f8767d5c95d
Ioana21-b <56729761+Ioana21-b@users.noreply.github.com>
2021-03-26 17:42:06 +0100

    Creazione Repository MovimentiScaffale #5
    Creazione Repository MovimentiScaffale #5

---
+++ scarsefour-server/src/main/java/it/iad2/scarsefourserver/repository/MovimentiScaffaleRepository.java
@@ -0,0 +1,13 @@
+package it.iad2.scarsefourserver.repository;
+
+import it.iad2.scarsefourserver.model.MovimentiScaffale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author utente
+ */
+public interface MovimentiScaffaleRepository extends JpaRepository<MovimentiScaffale, Long> {
+    
+    
+}
