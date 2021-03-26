fae0bcbc2e1da9edad6ff1905b7cd58a47c72060
Francesco Marro <chicco.marro@gmail.com>
2021-03-26 16:33:32 +0100

    Implementata Entity Scontrino #5
    Implementata Entity Scontrino #5

---
+++ scarsefour-server/src/main/java/it/iad2/scarsefourserver/model/Scontrino.java
@@ -0,0 +1,74 @@
+package it.iad2.scarsefourserver.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+public class Scontrino {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private LocalDateTime timeStamp;
+
+    @Column
+    private Integer numero;
+
+    @Column
+    private Double totale;
+
+    public Scontrino() {
+    }
+
+    public Scontrino(LocalDateTime timeStamp, Integer numero, Double totale) {
+        this.timeStamp = timeStamp;
+        this.numero = numero;
+        this.totale = totale;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public Double getTotale() {
+        return totale;
+    }
+
+    public void setTotale(Double totale) {
+        this.totale = totale;
+    }
+
+    @Override
+    public String toString() {
+        return "Scontrino{" +
+                "id=" + id +
+                ", timeStamp=" + timeStamp +
+                ", numero=" + numero +
+                ", totale=" + totale +
+                '}';
+    }
+}
---
+++ scarsefour-server/src/main/java/it/iad2/scarsefourserver/repository/ScontrinoRepository.java
@@ -0,0 +1,7 @@
+package it.iad2.scarsefourserver.repository;
+
+import it.iad2.scarsefourserver.model.Scontrino;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScontrinoRepository extends JpaRepository<Scontrino, Long> {
+}
