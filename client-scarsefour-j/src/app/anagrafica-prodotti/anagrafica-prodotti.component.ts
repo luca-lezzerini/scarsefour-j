d23a806c3951cd1a6162a35c809aef9c7718cf81
Valerio1990 <valeriopiccioni1990@gmail.com>
2021-03-26 16:20:43 +0100

    Richiesta creazione componente anagrafica prodotti #17

---
+++ client-scarsefour-j/src/app/anagrafica-prodotti/anagrafica-prodotti.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-anagrafica-prodotti',
+  templateUrl: './anagrafica-prodotti.component.html',
+  styleUrls: ['./anagrafica-prodotti.component.css']
+})
+export class AnagraficaProdottiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
