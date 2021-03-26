1fa6fed2205eac5f71b15d43398898e0634665be
Ioana21-b <56729761+Ioana21-b@users.noreply.github.com>
2021-03-26 17:46:23 +0100

    Creazione Repository SkuScaffale #5
    Creazione Respository Sabatini di SkuScaffale (non può dal suo perchè ha il Mac) #5

---
+++ scarsefour-server/src/main/java/it/iad2/scarsefourserver/repository/SkuScaffaleRepository.java
@@ -0,0 +1,12 @@
+package it.iad2.scarsefourserver.repository;
+
+import it.iad2.scarsefourserver.model.SkuScaffale;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author utente
+ */
+public interface SkuScaffaleRepository extends JpaRepository<SkuScaffale, Long> {
+    
+}
