cf5d3c0b71d67721853d5f0fdd56b9dd90b2c2e3
mainieri@tm <mainieri.tm@programmer.net>
2021-03-27 22:25:34 +0100

    Commit iniziale ticket #15
    Creato scheletro interfaccia utente

--- client-scarsefour-j/src/app/dashboard-gruppo-tre/dashboard-gruppo-tre.component.ts
+++ client-scarsefour-j/src/app/dashboard-gruppo-tre/dashboard-gruppo-tre.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Prodotto } from '../entità/prodotto';
 
 @Component({
   selector: 'app-dashboard-gruppo-tre',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardGruppoTreComponent implements OnInit {
 
+  barcode: string;
+  // riportare l'ultimo elemento della tabella, solo descrizione e prezzo
+  ultimoElemento: string;
+  prodotto: Prodotto = new Prodotto();
+  prodotti: Prodotto [] = [];
+  // inserire nelle entità del client, scontrino e rigascontrino
+  // scontrino: Scontrino= new Scontrino();
+  // rigaScontrino: RigaScontrino = new RigaScontrino();
+  prezzoProdotto: number;
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  // aggiugni il numero del gruppo alle chiamate al metodo
+  vediPrezzo() {
+
+  }
+
+  chiudiScontrino() {
+
+  }
+
+  stornaUltimo() {
+
+  }
+  annullaScontrino() {
+
+  }
+  annulla() {
+
+  }
+  conferma() {
+
+  }
+
 
 }
