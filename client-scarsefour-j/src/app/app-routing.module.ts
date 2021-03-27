118792766e24a2c8212a43ca64cd27c1d528e46d
Roberta Conti <r.conti70@gmail.com>
2021-03-27 12:24:23 +0100

    Risoluzione parziale ticket #18

--- client-scarsefour-j/src/app/app-routing.module.ts
+++ client-scarsefour-j/src/app/app-routing.module.ts
@@ -1,9 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AnagraficaCassiereComponent } from './anagrafica-cassiere/anagrafica-cassiere.component';
+import { AnagraficaPosizioniComponent } from './anagrafica-posizioni/anagrafica-posizioni.component';
 import { AnagraficaProdottiComponent } from './anagrafica-prodotti/anagrafica-prodotti.component';
+import { AnagraficaScontiComponent } from './anagrafica-sconti/anagrafica-sconti.component';
+import { DashboardGruppoTreComponent } from './dashboard-gruppo-tre/dashboard-gruppo-tre.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
+ { path:'anagrafica-casse', component:AnagraficaProdottiComponent},
+ { path:'anagrafica-cassiere', component:AnagraficaCassiereComponent},
+ { path:'anagrafica-posizioni', component:AnagraficaPosizioniComponent},
  { path:'anagrafica-prodotti', component:AnagraficaProdottiComponent},
+ { path:'anagrafica-sconti', component:AnagraficaScontiComponent},
+ { path:'dashboard-gruppo-tre', component:DashboardGruppoTreComponent},
+ { path:'home-page', component:HomePageComponent},
+ { path:"", redirectTo:"/home-page", pathMatch: "full"}
+ 
 ];
 
 @NgModule({
