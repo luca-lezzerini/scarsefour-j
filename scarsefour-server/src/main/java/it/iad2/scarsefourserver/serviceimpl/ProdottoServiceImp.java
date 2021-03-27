51b84dad628e13d183577e4832a40ee7fa0d7eef
Valerio1990 <valeriopiccioni1990@gmail.com>
2021-03-27 12:13:01 +0100

    #8

--- scarsefour-server/src/main/java/it/iad2/scarsefourserver/serviceimpl/ProdottoServiceImp.java
+++ scarsefour-server/src/main/java/it/iad2/scarsefourserver/serviceimpl/ProdottoServiceImp.java
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ProdottoServiceImpl implements ProdottoService {
+public class ProdottoServiceImp implements ProdottoService {
 
     @Autowired
     ProdottoRepository prodottoRepository;
