9ec42e4a15a648663830bf91f6089264f5340da6
danieleSantini <danielesantini94@gmail.com>
2021-03-28 19:54:34 +0200

    #16
    implementazione iniziale automa

---
+++ client-scarsefour-j/src/app/dashboard-gruppo-quattro/eventi-gruppo-quattro.ts
@@ -0,0 +1,24 @@
+import { Scontrino } from "../entità/scontrino";
+import { EventGruppoQuattro } from "./event-gruppo-quattro";
+
+export class VediPrezzoEvent implements EventGruppoQuattro{}
+export class EanEvent implements EventGruppoQuattro{
+    codiceEan:string;
+    scontrino:Scontrino = new Scontrino();
+
+    constructor(codiceEan:string, scontrino:Scontrino){
+        this.codiceEan = codiceEan;
+        this.scontrino = scontrino;
+    }
+}
+export class ChiudiEvent implements EventGruppoQuattro{}
+export class StornaEvent implements EventGruppoQuattro{
+    element:number;
+
+    constructor(element:number){
+        this.element = element;
+    }
+}
+export class ConfermaEvent implements EventGruppoQuattro{}
+export class AnnullaEvent implements EventGruppoQuattro{}
+export class AnnullaScontrinoEvent implements EventGruppoQuattro{}
