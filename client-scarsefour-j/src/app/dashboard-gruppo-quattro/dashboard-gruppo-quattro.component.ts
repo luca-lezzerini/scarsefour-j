e25c18382606b99ce6f718ccb1f10e3e4d8c75d1
AlbertoMartinezMontero <79788629+AlbertoMartinezMontero@users.noreply.github.com>
2021-03-29 11:44:40 +0200

    #16
    Implementazione parziale client

--- client-scarsefour-j/src/app/dashboard-gruppo-quattro/dashboard-gruppo-quattro.component.ts
+++ client-scarsefour-j/src/app/dashboard-gruppo-quattro/dashboard-gruppo-quattro.component.ts
@@ -1,58 +1,129 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ConfermaEvent } from '../automa/eventi';
+import { Prodotto } from '../entità/prodotto';
+import { Scontrino } from '../entità/scontrino';
+import { AutomaGruppoQuattro } from './automa/automa-gruppo-quattro';
+import { AnnullaEvent, AnnullaScontrinoEvent, ChiudiEvent, EanEvent, StornaEvent, VediPrezzoEvent } from './automa/eventi-gruppo-quattro';
 
 @Component({
   selector: 'app-dashboard-gruppo-quattro',
   templateUrl: './dashboard-gruppo-quattro.component.html',
-  styleUrls: ['./dashboard-gruppo-quattro.component.css']
+  styleUrls: ['../theme.css']
 })
 export class DashboardGruppoQuattroComponent implements OnInit {
-  
-  ean:string;
-  Descrizione:string;
-  prezzo:string;
-  totale:string;
-  scontrini:string;
 
-  eanNonEditabile:boolean;
-  vediPrezzoVisibleB:boolean;
-  chiudiB:boolean;
-  stornaB:boolean;
-  annullaScontrinoB:boolean;
-  annullaB:boolean;
-  confermaB:boolean;
-  prezzoB:boolean;
-  lista:boolean;
-  
-  constructor() { }
+  ean: string;
+  Descrizione: string;
+  prezzo: string;
+  totale: string;
+  scontrini: string;
+  automa: AutomaGruppoQuattro;
+  scontrino:Scontrino;
+  prodotto:Prodotto;
+
+  eanNonEditabile: boolean;
+  vediPrezzoVisibleB: boolean;
+  chiudiB: boolean;
+  stornaB: boolean;
+  annullaScontrinoB: boolean;
+  annullaB: boolean;
+  confermaB: boolean;
+  prezzoB: boolean;
+  lista: boolean;
+  prezzoLabel: boolean;
+  totaleScontrinoLabel:boolean;
+  tastoTabMes:boolean;
+  constructor(http: HttpClient, private router: Router) {
+   }
 
   ngOnInit(): void {
+    this.automa = new AutomaGruppoQuattro(this);
+  }
+
+  goToScontrinoVuoto() {
+    this.eanNonEditabile = false;
+    this.vediPrezzoVisibleB = true;
+    this.chiudiB = false
+    this.stornaB = false
+    this.annullaScontrinoB = false
+    this.annullaB = false;
+    this.confermaB = false;
+    this.prezzoB = false;
+    this.lista = false;
+    this.prezzoLabel = false;
+    this.totaleScontrinoLabel=false;
+    this.tastoTabMes=true;
+  }
+  goToVediPrezzo() {
+    this.eanNonEditabile = false;
+    this.vediPrezzoVisibleB = false;
+    this.chiudiB = false;
+    this.stornaB = false;
+    this.annullaScontrinoB = false;
+    this.annullaB = false;
+    this.confermaB = false;
+    this.prezzoB = false;
+    this.lista = false;
+    this.tastoTabMes=false;
+  }
+  gotoScontrinoNonVuoto() {
+    this.eanNonEditabile = false;
+    this.vediPrezzoVisibleB = true;
+    this.chiudiB = true;
+    this.stornaB = true;
+    this.annullaScontrinoB = true;
+    this.annullaB = false;
+    this.confermaB = false;
+    this.prezzoB = true;
+    this.lista = true;
+    this.tastoTabMes=true;
+  }
+  gotoAnnullamentoScontrino() {
+    this.eanNonEditabile = false;
+    this.vediPrezzoVisibleB = false;
+    this.chiudiB = false;
+    this.stornaB = false;
+    this.annullaScontrinoB = true;
+    this.annullaB = true;
+    this.confermaB = true;
+    this.prezzoB = false;
+    this.lista = false;
+    this.tastoTabMes=false;
+  }
+
+  vediPrezzo() {
+    this.automa.next(new VediPrezzoEvent());
   }
 
 
-  vediPrezzo(){
+  chiudiScontrino() {
+    this.automa.next(new ChiudiEvent());
 
   }
 
-  chiudiScontrino(){
+  stornaUltimo() {
+
+    this.automa.next(new StornaEvent(1));
 
   }
 
-  stornaUltimo(){
+  annullaScontrino() {
+    this.automa.next(new AnnullaScontrinoEvent());
 
   }
 
-  annullaScontrino(){
+  Annulla() {
+    this.automa.next(new AnnullaEvent());
 
   }
 
-  Annulla(){
-
+  Conferma() {
+    this.automa.next(new ConfermaEvent());
   }
-
-  Conferma(){
-
+  inserisceEan(){
+    this.automa.next(new EanEvent(this.ean, this.scontrino));
   }
-
-
-
+  
 }
