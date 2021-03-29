eff258439a532947b11ada4a0612270b70f299bf
AulicinoADiego <aulicinodiego@gmail.com>
2021-03-29 16:26:11 +0200

    Commit Temporaneo

--- client-scarsefour-j/src/app/dashboard-gruppo-uno/automa-dashboard-uno/stati-dashboard-uno.ts
+++ client-scarsefour-j/src/app/dashboard-gruppo-uno/automa-dashboard-uno/stati-dashboard-uno.ts
@@ -8,15 +8,20 @@ export class ScontrinoVuotoState implements StateDashboardUno {
     constructor(public automa: AutomaDashboardUno) {
         if (automa.stato instanceof ScontrinoVuotoState) {
             automa.gui.goToScontrinoVuotoPrimoEan();
+            console.log("goToScontrinoVuotoPrimoEan()");
         } else if (automa.stato instanceof ScontrinoNonVuotoState) {
             automa.gui.goToScontrinoVuotoFromAll();
+            console.log("goToScontrinoVuotoFromAll()");
         } else if (automa.stato instanceof VediPrezzoState) {
             automa.gui.goToScontrinoVuotoFromAll();
+            console.log("goToScontrinoVuotoFromAll()");
         } else if (automa.stato instanceof AnnullamentoScontrinoState) {
             automa.gui.goToScontrinoVuotoFromAnnulla();
+            console.log("goToScontrinoVuotoFromAnnulla()");
         }
         else {
             automa.gui.goToScontrinoVuotoInitial();
+            console.log("goToScontrinoVuotoInitial()");
         }
     }
 
@@ -29,7 +34,9 @@ export class ScontrinoVuotoState implements StateDashboardUno {
                 return new ScontrinoNonVuotoState(this.automa);
             }
             else {
-                return new ScontrinoVuotoState(this.automa)
+                //this.automa.gui.goToScontrinoVuotoPrimoEan();
+                //console.log("sono in goToScontrinoVuotoPrimoEan")
+                return new ScontrinoVuotoState(this.automa);
             }
         }
         else {
@@ -47,10 +54,12 @@ export class ScontrinoNonVuotoState implements StateDashboardUno {
 
     next(e: EventDashboardUno): StateDashboardUno {
         if (e instanceof ChiudiEvent) {
+            //this.automa.gui.goToScontrinoVuotoFromAll();
             return new ScontrinoVuotoState(this.automa);
         }
         else if (e instanceof StornaEvent) {
             if (e.numElem == 1) {
+                //this.automa.gui.goToScontrinoVuotoFromAll();
                 return new ScontrinoVuotoState(this.automa);
             }
             else if (e.numElem > 1) {
@@ -92,9 +101,11 @@ export class VediPrezzoState implements StateDashboardUno {
                 return new ScontrinoNonVuotoState(this.automa);
             }
             else if (!e.scontrino && !e.codiceEan) {
+                //this.automa.gui.goToScontrinoVuotoFromAll();
                 return new ScontrinoVuotoState(this.automa);
             }
             else if (!e.scontrino && e.codiceEan) {
+                //this.automa.gui.goToScontrinoVuotoFromAll();
                 return new ScontrinoVuotoState(this.automa);
             }
         }
@@ -116,6 +127,7 @@ export class AnnullamentoScontrinoState implements StateDashboardUno {
             return new ScontrinoNonVuotoState(this.automa);
         }
         else if (e instanceof ConfermaEvent) {
+            //this.automa.gui.goToScontrinoVuotoFromAnnulla();
             return new ScontrinoVuotoState(this.automa);
         }
         else {
---
+++ client-scarsefour-j/src/app/dashboard-gruppo-uno/dto-dashboard-uno/risposta-prezzo-dto.ts
@@ -0,0 +1,3 @@
+export class RispostaPrezzoDto{
+    prezzo : number;
+}
