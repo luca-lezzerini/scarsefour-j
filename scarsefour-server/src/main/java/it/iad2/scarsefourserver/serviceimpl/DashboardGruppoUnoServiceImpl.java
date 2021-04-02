9b9e4001eaa96c04d054d1262c71cf30eb90f1f2
Roberta Conti <r.conti70@gmail.com>
2021-04-02 15:14:52 +0200

    Risolto ticket  #48

--- scarsefour-server/src/main/java/it/iad2/scarsefourserver/serviceimpl/DashboardGruppoUnoServiceImpl.java
+++ scarsefour-server/src/main/java/it/iad2/scarsefourserver/serviceimpl/DashboardGruppoUnoServiceImpl.java
@@ -28,16 +28,16 @@ public class DashboardGruppoUnoServiceImpl implements DashboardGruppoUnoService 
     public RispostaEanDto cercaEan1(String barcode, Scontrino scontrino) {
         RispostaEanDto rispostaEanDto = new RispostaEanDto();
         System.out.println("barcode: " + barcode);
-        //cerco se esiste un prodotto nel repository con il barcode passato in input
+        //verifico l'esistenza di un prodotto nel repository con il barcode passato in input
         Prodotto prodotto = prodottoRepository.findByEanEquals(barcode);
         if (prodotto == null) {
-            //prodotto non trovato
+            //Se prodotto non trovato stampo a console messaggio di errore preparo i dati da inviare al client
             System.out.println("Errore EAN sconosciuto");
+            //Preparo i dati da rinviare al client
             rispostaEanDto.setScontrino(scontrino);
             if (scontrino != null) {
                 rispostaEanDto.setRigheScontrino(scontrinoRepository.findById(scontrino.getId()).get().getRighe());
             }
-
         } else {
             //prodotto trovato
             if (scontrino == null) {
@@ -53,21 +53,29 @@ public class DashboardGruppoUnoServiceImpl implements DashboardGruppoUnoService 
                 scontrino.setTotale(scontrino.getTotale() + prodotto.getPrezzo());
                 scontrino.getRighe().add(riga);
                 scontrinoRepository.save(scontrino);
-                // preparo la risposta
+                //Preparo i dati da rinviare al client
                 rispostaEanDto.setBarcode(barcode);
                 rispostaEanDto.setScontrino(scontrino);
                 rispostaEanDto.setRigheScontrino(scontrinoRepository.findById(scontrino.getId()).get().getRighe());
             } else {
+                // lo scontrino è esistente:
+                // carico la lista delle righe associate allo scontrino
                 List<RigaScontrino> listaRighe = scontrinoRepository.findById(scontrino.getId()).get().getRighe();
                 boolean trovato = false;
+                // scorro la lista
                 for (RigaScontrino riga : listaRighe) {
+                    // verifico se il prodotto in esame è già presente nell'elenco
                     if (riga.getProdotto().getEan().equals(barcode)) {
+                        //se già presente, incremento la quantità del prodotto                        
                         riga.setQuantita(riga.getQuantita() + 1);
+                        //aggiorno il totale dello scontrino sommandogli il preddo del prodotto 
                         scontrino.setTotale(scontrino.getTotale() + prodotto.getPrezzo());
                         scontrino.setRighe(listaRighe);
+                        //aggiorno il database 
                         rigaScontrinoRepository.save(riga);
                         scontrinoRepository.save(scontrino);
                         trovato = true;
+                        //Preparo i dati da rinviare al client
                         rispostaEanDto.setBarcode(barcode);
                         rispostaEanDto.setScontrino(scontrino);
                         rispostaEanDto.setRigheScontrino(listaRighe);
@@ -75,6 +83,7 @@ public class DashboardGruppoUnoServiceImpl implements DashboardGruppoUnoService 
                     }
                 }
                 if (!trovato) {
+                    //il prodotto in esame non è già presente nello scontrino
                     listaRighe = scontrinoRepository.findById(scontrino.getId()).get().getRighe();
                     // salvo nel repository una nuova rigaScontrino
                     RigaScontrino riga = new RigaScontrino();
@@ -86,7 +95,7 @@ public class DashboardGruppoUnoServiceImpl implements DashboardGruppoUnoService 
                     scontrino.setTotale(scontrino.getTotale() + prodotto.getPrezzo());
                     scontrino.setRighe(listaRighe);
                     scontrinoRepository.save(scontrino);
-                    // preparo la risposta
+                    //Preparo i dati da rinviare al client
                     rispostaEanDto.setBarcode(barcode);
                     rispostaEanDto.setScontrino(scontrino);
                     rispostaEanDto.setRigheScontrino(listaRighe);
@@ -99,13 +108,16 @@ public class DashboardGruppoUnoServiceImpl implements DashboardGruppoUnoService 
 
     @Override
     public Scontrino chiudiScontrino1(Scontrino scontrino) {
+        //chiudo lo scontrino assegnandogli un numero progressivo continuativo
         scontrino.setNumero(scontrinoRepository.findAll().size() + 1);
+        //aggiorno il database 
         scontrinoRepository.save(scontrino);
         return null;
     }
 
     @Override
     public double vediPrezzo1(String barcode) {
+        //prendo dal DB i dati relativi al prodotto in esame e rimando il prezzo 
         Prodotto prodotto = prodottoRepository.findByEanEquals(barcode);
         if (prodotto != null) {
             return prodotto.getPrezzo();
@@ -115,15 +127,20 @@ public class DashboardGruppoUnoServiceImpl implements DashboardGruppoUnoService 
 
     @Override
     public ScontrinoRigheDto stornaUltimo1(Scontrino scontrino, String lastBarcode) {
+        //seleziono tutte le righe dello scontrino 
         List<RigaScontrino> listaRighe = scontrinoRepository.findById(scontrino.getId()).get().getRighe();
-
+        //scorro tutta la lista
         for (RigaScontrino riga : listaRighe) {
+            // verifico se il prodotto della lista che sto scorrendo corrispondesse all'ultimo prodotto da cancellare
             if (riga.getProdotto().getEan().equals(lastBarcode)) {
+                // se corrisponde sottraggo l'imporo del prodotto dal totale
                 scontrino.setTotale(scontrino.getTotale() - riga.getProdotto().getPrezzo());
+                // se il prodotto ha più di un elemento decremento solo la quantità e aggiorno la tabella RigaScontrino
                 if (riga.getQuantita() > 1) {
                     riga.setQuantita(riga.getQuantita() - 1);
                     rigaScontrinoRepository.save(riga);
                 } else {
+                    //cancello il prodotto  
                     rigaScontrinoRepository.delete(riga);
                     listaRighe.remove(riga);
                     scontrino.setRighe(listaRighe);
@@ -137,10 +154,12 @@ public class DashboardGruppoUnoServiceImpl implements DashboardGruppoUnoService 
 
     @Override
     public Scontrino annullaScontrino1(Scontrino scontrino) {
+        //seleziono tutte le righe dello scontrino da cancellare che andrò a cancellare dalla tabella RigaScontrino
         List<RigaScontrino> listaRighe = scontrinoRepository.findById(scontrino.getId()).get().getRighe();
         listaRighe.forEach(x -> {
             rigaScontrinoRepository.delete(x);
         });
+        //Cancello lo scontrino nella tabella Scontrino
         scontrinoRepository.delete(scontrino);
         return null;
     }
