2e603d7b800668a5a98d0421839fd082a07fbdad
Angelita Doria <angiegiuse@gmail.com>
2021-04-10 19:58:40 +0200

    Update AnagraficaPosizioneScaffaleService.java
    risolto ticket #52

--- scarsefour-server/src/main/java/it/iad2/scarsefourserver/service/AnagraficaPosizioneScaffaleService.java
+++ scarsefour-server/src/main/java/it/iad2/scarsefourserver/service/AnagraficaPosizioneScaffaleService.java
@@ -5,13 +5,40 @@ import java.util.List;
 
 public interface AnagraficaPosizioneScaffaleService {
 
+    /**
+     * Consente di effettuare una ricerca per codice di una posizione scaffale 
+     * @param s criterio di ricerca per codice posizione
+     * @return lista delle posizioni scaffale presenti in archivio
+     */
     List<PosizioneScaffale> ricerca(String s);
 
+    /**
+     * Rimuove una posizione scaffale dai dati in archivio
+     * @param posizione entità di tipo 
+     * @see PosizioneScaffale
+     * @return lista delle posizioni scaffale aggiornate
+     */
     List<PosizioneScaffale> rimuovi(PosizioneScaffale posizione);
 
+    /**
+     * Modifica una posizione scaffale dai dati in archivio
+     * @param posizione entità di tipo
+     * @see PosizioneScaffale
+     * @return lista delle posizioni scaffale aggiornate
+     */
     List<PosizioneScaffale> modifica(PosizioneScaffale posizione);
 
+    /**
+     * Aggiunge una posizione scaffale ai dati in archivio
+     * @param posizione di tipo
+     * @see PosizioneScaffale
+     * @return lista di posizioni scaffale aggiornate
+     */
     List<PosizioneScaffale> aggiungi(PosizioneScaffale posizione);
     
+    /**
+     * Restituisce la lista di posizioni scaffale presente in archivio
+     * @return lista di posizioni scaffale
+     */
     List<PosizioneScaffale> aggiorna();
 }
