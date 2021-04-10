651bf213d372ef93f16a3a162da892d0f6e781e4
Angelita Doria <angiegiuse@gmail.com>
2021-04-10 20:17:13 +0200

    Update DashboardTreService.java
    Aggiunto javadoc ticket #47

--- scarsefour-server/src/main/java/it/iad2/scarsefourserver/service/DashboardTreService.java
+++ scarsefour-server/src/main/java/it/iad2/scarsefourserver/service/DashboardTreService.java
@@ -9,21 +9,70 @@ import java.util.List;
 
 public interface DashboardTreService {
 
+    /**
+     * Consente di ricercare un prodotto per codice ean
+     * @param ean criterio di ricerca
+     * @return restituisce un Prodotto
+     * @see Prodotto
+     */
     Prodotto vediPrezzo(String ean);
 
+    /**
+     * Consente di associare e salvare uno scontrino e le righeScontrino
+     * crea uno scontrino nuovo e vuoto
+     * @param scontrino di tipo Scontrino
+     * @see Scontrino
+     * @param righe una lista di RigaScontrino
+     * @see RigaScontrino
+     * @return uno scontrino vuoto
+     */
     Scontrino chiudiScontrino(Scontrino scontrino, List<RigaScontrino> righe);
 
+    /**
+     * Cancella tutte le righe associate allo scontrino in ingresso
+     * @param scontrino di tipo Scontrino
+     * @see Scontrino
+     * @return lo scontrino con la lista di righe vuota
+     */
     Scontrino annullaScontrino(Scontrino scontrino);
-//    List<RigaScontrino> stornaUltimo(Scontrino scontrino);
-//    List<RigaScontrino> aggiungiRigaScontrino(Scontrino scontrino);
-//    List<RigaScontrino> aggiornaRighe(Scontrino scontrino);
 
+    /**
+     * Rimuove l'ultima riga scontrino associata allo scontrino in ingresso
+     * e ricalcola il totale dello scontrino
+     * @param scontrino di tipo Scontrino
+     * @see Scontrino
+     * @return una lista di RigaScontrino
+     * @see RigaScontrino
+     */
     ListaRigaScontrinoTreDto stornaUltimo(Scontrino scontrino);
 
+    /**
+     * Aggiunge una riga Scontrino allo scontrino in ingresso e ricalcola il 
+     * totale dello scontrino, salva lo scontrino modificato
+     * @param scontrino di tipo Scontrino
+     * @see Scontrino
+     * @see RigaScontrino
+     * @param ean barcode del prodotto per la ricerca del prodotto da associare
+     * alla riga scontrino
+     * @return un AggiungiEanRispostaDto che contiene lo scontrino, 
+     * le righe scontrino e l'esito dell'operazione
+     * @see AggiungiEanRispostaDto
+     */
     AggiungiEanRispostaDto aggiungiRigaScontrino(Scontrino scontrino, String ean);
 
+    /**
+     * Restituisce le righe associate allo scontrino
+     * @param scontrino di tipo Scontrino
+     * @see Scontrino
+     * @return uno Scontrino con le righe Scontrino
+     */
     Scontrino aggiornaRighe(Scontrino scontrino);
     
+    /**
+     * Crea un nuovo scontrino vuoto
+     * @return scontrino di tipo Scontrino
+     * @see Scontrino
+     */
     Scontrino creaNuovoScontrinoVuoto();
     
 }
