6b062d93c894b041f8d061863d3ed27233b6140b
mainieri@tm <mainieri.tm@programmer.net>
2021-04-11 19:37:38 +0200

    Commit parziale #77

--- client-scarsefour-j/src/app/entità/sku-scaffale.ts
+++ client-scarsefour-j/src/app/entità/sku-scaffale.ts
@@ -1,5 +1,10 @@
+import { PosizioneScaffale } from "./posizione-scaffale";
+import { Prodotto } from "./prodotto";
+
 export class SkuScaffale{
     id: number;
     giacenza: number;
     scortaMinima: number;
+    prodotti: Prodotto[]=[];
+    posizioni: PosizioneScaffale[]=[];
 }
--- client-scarsefour-j/src/app/visualizza-giacenza/visualizza-giacenza.component.ts
+++ client-scarsefour-j/src/app/visualizza-giacenza/visualizza-giacenza.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { PosizioneScaffale } from '../entità/posizione-scaffale';
+import { SkuScaffale } from '../entità/sku-scaffale';
+import { ListaPosizioneScaffaleDto } from '../dto/lista-posizione-scaffale-dto';
+import { HttpClient } from '@angular/common/http';
+import { PosizioneScaffaleDto } from '../dto/posizione-scaffale-dto';
+import { ListaProdottiDto } from '../dto/lista-prodotti-dto';
+import { Prodotto } from '../entità/prodotto';
 
 @Component({
   selector: 'app-visualizza-giacenza',
@@ -8,18 +15,39 @@ import { PosizioneScaffale } from '../entità/posizione-scaffale';
 })
 export class VisualizzaGiacenzaComponent implements OnInit {
 
-  posizioni: PosizioneScaffale[]= [];
-  
+  posizioni: PosizioneScaffale[] = [];
+  sku: SkuScaffale[] = [];
+  prodotti: Prodotto[] = [];
+  posizione: PosizioneScaffale;
+
   //variabili di visibilità
-  tablePosizioniVisible: boolean;
-  divPosizioniVisible: boolean;
+  // tablePosizioniVisible: boolean = false;
+  // divPosizioniVisible: boolean = false;
   formDivVisible: boolean;
   formVisible: boolean;
 
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.aggiornaPosizioni();
+  }
 
   ngOnInit(): void {
   }
+
+  aggiornaPosizioni() {
+    let oss: Observable<ListaPosizioneScaffaleDto> = this.http.get<ListaPosizioneScaffaleDto>(
+      'http://localhost:8080/aggiorna-posizioni-giacenza');
+      oss.subscribe((c) => (this.posizioni = c.listaPosizioni));
+  }
+
+  // visualizzaGiacenza() { 
+  //   let dto : PosizioneScaffaleDto = new PosizioneScaffaleDto();
+  //   dto.posizione = this.posizione;
+  //   let oss: Observable<ListaProdottiDto> = this.http.get<ListaProdottiDto>(
+  //     'http://localhost:8080/visualizza-giacenza');
+  //     oss.subscribe(p =>{
+  //       this.prodotti=p.listaProdotti;
+  //     });
+  // }
 
 }
