9f525ecac72663ac32c7148d51d263628cbf5288
mainieri@tm <mainieri.tm@programmer.net>
2021-04-11 18:54:05 +0200

    Commit parziale #77
    Implementato html

--- client-scarsefour-j/src/app/visualizza-giacenza/visualizza-giacenza.component.ts
+++ client-scarsefour-j/src/app/visualizza-giacenza/visualizza-giacenza.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { PosizioneScaffale } from '../entità/posizione-scaffale';
 
 @Component({
   selector: 'app-visualizza-giacenza',
   templateUrl: './visualizza-giacenza.component.html',
-  styleUrls: ['./visualizza-giacenza.component.css']
+  styleUrls: ['../theme.css']
 })
 export class VisualizzaGiacenzaComponent implements OnInit {
+
+  posizioni: PosizioneScaffale[]= [];
+  
+  //variabili di visibilità
+  tablePosizioniVisible: boolean;
+  divPosizioniVisible: boolean;
+  formDivVisible: boolean;
+  formVisible: boolean;
+
 
   constructor() { }
 
