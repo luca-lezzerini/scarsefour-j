1ff56c87a02652affac0e0ec33eaee6c9938dfd4
DESKTOP-J5C4BKM\Samuele <5>
2021-04-12 17:20:04 +0200

    create classi e interfacce ticket #79

--- scarsefour-server/src/main/java/it/iad2/scarsefourserver/repository/ProdottoRepository.java
+++ scarsefour-server/src/main/java/it/iad2/scarsefourserver/repository/ProdottoRepository.java
@@ -1,6 +1,7 @@
 package it.iad2.scarsefourserver.repository;
 
 import it.iad2.scarsefourserver.model.Prodotto;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
---
+++ scarsefour-server/src/main/java/it/iad2/scarsefourserver/service/ScaricaMerceInPosizioneService.java
@@ -0,0 +1,8 @@
+package it.iad2.scarsefourserver.service;
+
+import it.iad2.scarsefourserver.model.Prodotto;
+import java.util.List;
+
+public interface ScaricaMerceInPosizioneService {
+    List<Prodotto> mostraProdottiScaffale();
+}
