cacb8be84f782b5e2bf20cd6bee821ae4760ac3b
DESKTOP-J5C4BKM\Samuele <5>
2021-04-13 12:27:30 +0200

    implementato metodo cercaScaffale() lato client ticket #79
    implementato metodo cercaScaffale() lato client ticket #79

--- client-scarsefour-j/src/app/scarica-merce/scarica-merce.component.ts
+++ client-scarsefour-j/src/app/scarica-merce/scarica-merce.component.ts
@@ -1,22 +1,40 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { EsitoDtoDue } from '../dto/esito-dto-due';
+import { PosizioneScaffaleDto } from '../dto/posizione-scaffale-dto';
+import { PosizioneScaffale } from '../entità/posizione-scaffale';
 import { Prodotto } from '../entità/prodotto';
 
 @Component({
   selector: 'app-scarica-merce',
   templateUrl: './scarica-merce.component.html',
-  styleUrls: ['./scarica-merce.component.css']
+  styleUrls: ['../theme.css']
 })
 
 export class ScaricaMerceComponent implements OnInit {
   inserisciScaffale: string;
   prodotti: Prodotto[] = [];
 
-  constructor() { }
+  posizione: PosizioneScaffale;
+  esito: boolean;
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
   
-  cercaScaffale():void {}
+  cercaScaffale():void {
+    const dto: PosizioneScaffaleDto = new PosizioneScaffaleDto();
+    dto.posizione = this.posizione;
+    const oss: Observable<EsitoDtoDue> = this.http
+    .post<EsitoDtoDue>('http://localhost:8080/cerca-posizione-scaffale', dto);
+    oss.subscribe(p => {
+      this.esito = p.esito;
+      //ci vuole un cambiamento di stato????
+    });
+  }
+
   selezionaProdotto():void {}
 
 }
