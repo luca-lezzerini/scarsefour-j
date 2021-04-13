35ece7d460a199edda9f59148e8e4d9b726911e0
Angelita Doria <angiegiuse@gmail.com>
2021-04-13 11:46:27 +0200

    Risolto ticket #77

--- scarsefour-server/src/main/java/it/iad2/scarsefourserver/serviceimpl/VisualizzaGiacenzaServiceImpl.java
+++ scarsefour-server/src/main/java/it/iad2/scarsefourserver/serviceimpl/VisualizzaGiacenzaServiceImpl.java
@@ -3,8 +3,13 @@ package it.iad2.scarsefourserver.serviceimpl;
 import it.iad2.scarsefourserver.dto.ListaGiacenzaDto;
 import it.iad2.scarsefourserver.dto.ListaPosizioneScaffaleDto;
 import it.iad2.scarsefourserver.dto.PosizioneScaffaleDto;
+import it.iad2.scarsefourserver.model.Giacenza;
+import it.iad2.scarsefourserver.model.Prodotto;
+import it.iad2.scarsefourserver.model.SkuScaffale;
 import it.iad2.scarsefourserver.repository.PosizioneScaffaleRepository;
 import it.iad2.scarsefourserver.service.VisualizzaGiacenzaService;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,8 +26,20 @@ public class VisualizzaGiacenzaServiceImpl implements VisualizzaGiacenzaService 
 
     @Override
     public ListaGiacenzaDto visualizzaGiacenza(PosizioneScaffaleDto dto) {
+        List<Object[]> listaObj = posizioneScaffaleRepository.visualizzaGiacenzaProdotti(dto.getPosizione().getId());
+        List<Giacenza> listaGiacenza = new ArrayList<>();
+        for (Object o[] : listaObj) {
+            Prodotto p = (Prodotto) o[0];
+            SkuScaffale s = (SkuScaffale) o[1];
+            Giacenza g = new Giacenza();
+            g.setCodice(p.getCodice());
+            g.setDescrizione(p.getDescrizione());
+            g.setGiacenza(s.getGiacenza());
+            g.setScortaMinima(s.getScortaMinima());
+            listaGiacenza.add(g);
+        }
         //return new ListaGiacenzaDto(posizioneScaffaleRepository.visualizzaGiacenzaProdotti(dto.getPosizione().getId()));
-        return null;
+        return new ListaGiacenzaDto(listaGiacenza);
     }
 
 }
