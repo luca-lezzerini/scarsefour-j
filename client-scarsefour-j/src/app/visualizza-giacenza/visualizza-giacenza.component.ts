53e06c61542cf108b7f736d0995e14efa05b90e3
Angelita Doria <angiegiuse@gmail.com>
2021-04-16 10:35:21 +0200

    Commit parziale #77
    Aggiunta paginazione

--- client-scarsefour-j/src/app/visualizza-giacenza/visualizza-giacenza.component.ts
+++ client-scarsefour-j/src/app/visualizza-giacenza/visualizza-giacenza.component.ts
@@ -10,6 +10,8 @@ import { Prodotto } from '../entità/prodotto';
 import { ListaGiacenzaDto } from '../dto/lista-giacenza-dto';
 import { Giacenza } from '../entità/giacenza';
 import { CriterioRicercaDto } from '../dto/criterio-ricerca-dto';
+import { DatiPageDto } from '../dto/dati-page-dto';
+import { PageDto } from '../dto/page-dto';
 
 @Component({
   selector: 'app-visualizza-giacenza',
@@ -28,9 +30,21 @@ export class VisualizzaGiacenzaComponent implements OnInit {
   formDivVisible: boolean;
   formVisible: boolean;
 
+  //variabili paginazione
+  numPag: number = 0;
+  numPaginaV: number = 1;
+  elemPag: number = 10;
+  totalPages: number;
+  totalElements: number;
+  first: boolean;
+  last: boolean;
+
+  numberOfElements: number;
+
 
   constructor(private http: HttpClient) {
-    this.aggiornaPosizioni();
+    this.caricaPosizioniPaginati(this.numPaginaV);
+    console.log(this.numPaginaV);
   }
 
   ngOnInit(): void {
@@ -61,5 +75,84 @@ export class VisualizzaGiacenzaComponent implements OnInit {
     let oss: Observable<ListaPosizioneScaffaleDto> = this.http.post<ListaPosizioneScaffaleDto>('http://localhost:8080/ricerca-posizioni', dto);
     oss.subscribe(r => this.posizioni = r.listaPosizioni);
    }
+
+    //metodi paginazione
+  caricaPosizioniPaginati(numPaginaV: number) {
+    this.numPaginaV = numPaginaV;
+    console.log(numPaginaV);
+    if (this.numPaginaV > 0) {
+      let dto: DatiPageDto = new DatiPageDto();
+      dto.numPag = this.numPaginaV - 1;
+      dto.elemPag = this.elemPag;
+      let oss: Observable<PageDto> = this.http.post<PageDto>
+        ("http://localhost:8080/carica-posizioni-paginati", dto);
+      oss.subscribe(v => {
+        this.posizioni = v.listaElemPag.content;
+        this.totalPages = v.listaElemPag.totalPages;
+        this.numPag = this.numPaginaV - 1;
+        console.log("totalPages: " + v.listaElemPag.totalPages);
+        console.log("totalElements: " + v.listaElemPag.totalElements);
+        console.log("number: " + v.listaElemPag.number);
+        console.log("first: " + v.listaElemPag.first);
+        console.log("last: " + v.listaElemPag.last);
+        console.log("size. " + v.listaElemPag.size);
+        console.log("numberOfElements: " + v.listaElemPag.numberOfElements);
+        if (this.numPaginaV > this.totalPages) {
+          this.numPag = this.totalPages - 1;
+          this.numPaginaV = this.numPag + 1;
+          this.caricaPosizioniPaginati(this.numPaginaV);
+        }
+      });
+    }
+    else {
+      this.numPag = 0;
+      this.numPaginaV = this.numPag + 1;
+      this.caricaPosizioniPaginati(this.numPaginaV);
+    }
+  }
+
+  goToNext(numPaginaV: number) {
+    console.log("sono in next del padre");
+    console.log("val numPaginaV: ", numPaginaV);
+    this.numPaginaV = numPaginaV;
+    this.numPag += 1;
+    this.numPaginaV = this.numPag + 1;
+    this.caricaPosizioniPaginati(this.numPaginaV);
+  }
+
+  goToPrevious(numPaginaV: number) {
+    console.log("sono in prev del padre");
+    console.log("val numPaginaV: ", numPaginaV);
+    this.numPaginaV = numPaginaV;
+    this.numPag -= 1;
+    this.numPaginaV = this.numPag + 1;
+    this.caricaPosizioniPaginati(this.numPaginaV);
+  }
+
+  goToFirst(numPaginaV: number) {
+    console.log("sono in first del padre");
+    console.log("val numPaginaV: ", numPaginaV);
+    this.numPaginaV = numPaginaV;
+    this.numPag = numPaginaV - 1;
+    this.caricaPosizioniPaginati(this.numPaginaV);
+  }
+
+  goToLast(numPaginaV: number) {
+    console.log("sono in last del padre");
+    console.log("val numPaginaV: ", numPaginaV);
+    this.numPag = this.totalPages - 1;
+    this.numPaginaV = this.numPag + 1;
+    this.caricaPosizioniPaginati(this.numPaginaV);
+  }
+
+  setElemPag(elemPag: number) {
+    console.log("sono in setElemPage del padre")
+    this.elemPag = elemPag;
+    console.log("elemPag: ", elemPag);
+    this.numPag = 0;
+    this.numPaginaV = this.numPag + 1;
+    this.caricaPosizioniPaginati(this.numPaginaV);
+    //VISIBILITA??
+  }
 
 }
